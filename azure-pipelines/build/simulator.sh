a20605add5b2f7dc5a89416f6b1467dedd2ce453
arrkiin <arrkiin@gmail.com>
2019-07-15 14:40:56 +0200

    Changes to build a universal simulator build

--- azure-pipelines/build/simulator.sh
+++ azure-pipelines/build/simulator.sh
@@ -6,17 +6,15 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-for target in "release_debug" "release"; do
-  scons platform=iphone arch=x86_64 tools=no target=$target \
-        "${SCONS_FLAGS[@]}" "${SCONS_TMPL_FLAGS[@]}"
-done
+scons p=iphone tools=no target=release arch=arm "${SCONS_FLAGS[@]}" "${SCONS_TMPL_FLAGS[@]}"
+scons p=iphone tools=no target=release arch=arm64 "${SCONS_FLAGS[@]}" "${SCONS_TMPL_FLAGS[@]}"
+scons p=iphone tools=no target=release arch=x86_64 "${SCONS_FLAGS[@]}" "${SCONS_TMPL_FLAGS[@]}"
+lipo -create bin/libgodot.iphone.opt.arm.a bin/libgodot.iphone.opt.arm64.a bin/libgodot.iphone.opt.x86_64.a -output bin/godot.iphone.opt.universal.simulator.a
 
 # Create export templates ZIP archive
-mv "bin/libgodot.iphone.opt.debug.x86_64.a" "libgodot.iphone.debug.fat.a"
-mv "bin/libgodot.iphone.opt.x86_64.a" "libgodot.iphone.release.fat.a"
+mv "bin/godot.iphone.opt.universal.simulator.a" "libgodot.iphone.universal.simulator.a"
 touch "data.pck"
 zip -r9 \
     "$BUILD_ARTIFACTSTAGINGDIRECTORY/templates/simulator.zip" \
-    "libgodot.iphone.debug.fat.a" \
-    "libgodot.iphone.release.fat.a" \
+    "libgodot.iphone.universal.simulator.a" \
     "data.pck"
